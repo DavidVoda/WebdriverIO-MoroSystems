7301b758a3fcbd4854c1bb53a85891852389f53b
DavidVoda <44436248+DavidVoda@users.noreply.github.com>
2024-10-12 20:37:30 +0200

    Feature/example (#1)
    * init
    
    * test files
    
    * test files
    
    * fix ci fail
    
    * screenshot generation
    
    * changes to ci run
    
    * disabled cookies check
    
    * adjusted search to find czech page
    
    * enter key
    
    * enter key
    
    * language change
    
    * language change
    
    * fixed allure
    
    * fixed allure
    
    * fixed allure
    
    * typo
    
    * version
    
    * version
    
    ---------
    
    Co-authored-by: David Voda <voda.davd@gmail.com>

---
+++ test/specs/test.e2e.ts
@@ -0,0 +1,35 @@
+import GooglePage from '../pageobjects/google.page.ts'
+import MoroSystemsPage from '../pageobjects/morosystems.page.ts'
+import {browser} from "@wdio/globals";
+
+describe('MoroSystems test example', () => {
+    const googlePage = new GooglePage()
+    const moroSystemsPage = new MoroSystemsPage()
+
+    beforeEach(async ()=>{
+        await browser.setWindowSize(1500, 1000)
+        await browser.setTimeout({ 'pageLoad': 10000 })
+    })
+
+    it('Google MoroSystems and navigate to career page', async () => {
+        //Google
+        await googlePage.openPage()
+
+        // await googlePage.denyCookiesButton().click()
+        await googlePage.inputSearch().setValue('MoroSystems CZ')
+        await browser.keys('Enter')
+        // await googlePage.searchButton().click()
+
+        await expect(googlePage.moroSystemsLink()).toExist()
+        await googlePage.moroSystemsLink().click()
+
+        //MoroSystems
+        // await moroSystemsPage.denyCookies().click()
+        await moroSystemsPage.careerLink().waitForClickable()
+        await moroSystemsPage.careerLink().click()
+        await expect(browser).toHaveUrl('https://www.morosystems.cz/kariera/')
+        await expect(moroSystemsPage.openPositionsButton()).toExist()
+
+    })
+})
+
